e04f7b31d9ab6b2993d4d0d62ce3058d4a8c8291
Ethan <qiang.cao@uobgroup.com>
2024-04-23 17:34:40 +0800

    add start bean mornitor

---
+++ spring-creed-pom/spring-creed-pro/spring-creed-server/src/main/java/com/ethan/server/TimeCostBeanPostProcessor.java
@@ -0,0 +1,54 @@
+/*
+ * System Name         : GEBNexGen
+ * Program Id          : idbms
+ * Author              : venojk
+ * Date                : 23/4/24
+ * Copyright (c) United Overseas Bank Limited Co.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * United Overseas Bank Limited Co. ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in accordance
+ * with the terms of the license agreement you entered into with
+ * United Overseas Bank Limited Co.
+ */
+
+package com.ethan.server;
+
+import com.google.common.collect.Maps;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+/**
+ * @author EthanCao
+ * @description spring creed
+ * @date 23/4/24
+ *
+ * 监测 Spring boot 启动时间
+ * 通过 BeanPostProcessor 原理，在前置处理时记录下当前时间，在后置处理时，用当前时间减去前置处理时间，就能知道每个 Bean 的初始化耗时。
+ */
+@Component
+public class TimeCostBeanPostProcessor implements BeanPostProcessor {
+    private Map<String, Long> costMap = Maps.newConcurrentMap();
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        costMap.put(beanName, System.currentTimeMillis());
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        if (costMap.containsKey(beanName)) {
+            Long start = costMap.get(beanName);
+            long cost  = System.currentTimeMillis() - start;
+            if (cost > 0) {
+                costMap.put(beanName, cost);
+                System.out.println("bean: " + beanName + "\ttime: " + cost);
+            }
+        }
+        return bean;
+    }
+}
