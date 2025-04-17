e8bd333736d7d7be4fd3145acd5b8ff23fe68456
ethan <411084090@qq.com>
2025-04-17 21:50:28 +0800

    2025-02-20_json_schema_validator_update

--- spring-creed-example/spring-creed-propertysource-child/src/test/java/com/ethan/PropertySourceApplicationTest.java
+++ spring-creed-example/spring-creed-propertysource-child/src/test/java/com/ethan/PropertySourceApplicationTest.java
@@ -1,24 +1,3 @@
-/*
- * System Name         : GEBNexGen
- * Program Id          : gebng-cms
- * Program Description : Toast load jdbc ftl mapper test
- *
- * Revision History
- *
- * Date            Author             SR No           Description of Change
- * ----------      ----------         ------          ----------------------
- * Nov 15, 2019    Iynkaran				R5			  Initial commit
- *
- * Copyright (c) United Overseas Bank Limited Co.
- * All rights reserved.
- *
- * This software is the confidential and proprietary information of
- * United Overseas Bank Limited Co. ("Confidential Information").  You shall not
- * disclose such Confidential Information and shall use it only in accordance
- * with the terms of the license agreement you entered into with
- * United Overseas Bank Limited Co.
- */
-
 package com.ethan;
 
 import com.ethan.config.ExportProperties;
